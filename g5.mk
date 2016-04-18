c3663e8a9801193364742a85ba695f998568a9cc
Rashed Abdel-Tawab <rashed@linux.com>
2016-04-18 17:02:12 -0400

    g5: Add audio config files

--- g5.mk
+++ g5.mk
@@ -80,6 +80,12 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     tinymix
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8996 \
