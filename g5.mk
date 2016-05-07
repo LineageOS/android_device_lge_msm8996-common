b24a908bfa54cc998efc496a841565d908626410
Rashed Abdel-Tawab <rashed@linux.com>
2016-05-07 10:56:48 -0400

    g5: Build libxml2 (oops)

--- g5.mk
+++ g5.mk
@@ -217,6 +217,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8996.conf:system/etc/thermal-engine-8996.conf
 
+# Tools
+PRODUCT_PACKAGES += \
+    libxml2
+
 # USB
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
