afc3d46db2958383cfad9f53c9f1fe627c5dd39c
Rashed Abdel-Tawab <rashed@linux.com>
2016-05-08 00:40:13 -0400

    g5: Add GPS configs

--- g5.mk
+++ g5.mk
@@ -99,6 +99,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8996
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
