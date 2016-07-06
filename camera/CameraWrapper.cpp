4f66e60a092931e0931748273b4e20a8c61bfe7b
Rashed Abdel-Tawab <rashed@linux.com>
2016-07-06 10:24:24 -0400

    g5: camera: Add option to force number of cameras

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -590,7 +590,11 @@ static int camera_get_number_of_cameras(void)
     ALOGV("%s", __FUNCTION__);
     if (check_vendor_module())
         return 0;
+#ifdef FORCE_NUMBER_OF_CAMERAS
+    return FORCE_NUMBER_OF_CAMERAS;
+#else
     return gVendorModule->get_number_of_cameras();
+#endif
 }
 
 static int camera_get_camera_info(int camera_id, struct camera_info *info)
