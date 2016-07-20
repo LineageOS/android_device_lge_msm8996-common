803fe52c882849c8802a52013fa61fbf156f384f
Louis Popi <theh2o64@gmail.com>
2016-07-20 17:51:00 +0200

    g5: Update how blob lists are generated
     * Use the new generic extract utils in vendor/cm!

--- extract-files.sh
+++ extract-files.sh
@@ -1,97 +1,59 @@
 #!/bin/bash
-#set -x 
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-oat2dex()
-{
-    APK="$1"
+set -e
 
-    OAT="`dirname $APK`/arm/`basename $APK .apk`.odex"
-    if [ ! -e $OAT ]; then
-        return 0
-    fi
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-    HIT=`r2 -q -c '/ dex\n035' "$OAT" 2>/dev/null | grep hit0_0 | awk '{print $1}'`
-    if [ -z "$HIT" ]; then
-        echo "ERROR: Can't find dex header of `basename $APK`"
-        return 1
-    fi
+CM_ROOT="$MY_DIR"/../../..
 
-    SIZE=`r2 -e scr.color=false -q -c "px 4 @$HIT+32" $OAT 2>/dev/null | tail -n 1 | awk '{print $2 $3}' | sed -e "s/^/0x/" | rax2 -e`
-    r2 -q -c "pr $SIZE @$HIT > /tmp/classes.dex" "$OAT" 2>/dev/null
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Something went wrong in `basename $APK`"
-    fi
-}
-
-function extract() {
-    for FILE in `egrep -v '(^#|^$)' $1`; do
-        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-        DEST=${PARSING_ARRAY[1]}
-        if [ -z $DEST ]; then
-            DEST=$FILE
-        fi
-        DIR=`dirname $FILE`
-        if [ ! -d $2/$DIR ]; then
-            mkdir -p $2/$DIR
-        fi
-        if [ "$SRC" = "adb" ]; then
-            # Try CM target first
-            eval $ADB pull /system/$DEST $2/$DEST
-            # if file does not exist try OEM target
-            if [ "$?" != "0" ]; then
-                eval $ADB pull /system/$FILE $2/$DEST
-            fi
-            if [ "${FILE##*.}" = "apk" ]; then
-                oat2dex /system/$FILE
-            fi
-        else
-            cp $SRC/system/$FILE $2/$DEST
-            # if file dot not exist try destination
-            if [ "$?" != "0" ]
-                then
-                cp $SRC/system/$DEST $2/$DEST
-            fi
-            if [ "${FILE##*.}" = "apk" ]; then
-                oat2dex /system/$FILE
-            fi
-        fi
-
-        if [ -e /tmp/classes.dex ]; then
-            zip -gjq $2/$FILE /tmp/classes.dex
-            rm /tmp/classes.dex
-        fi
-    done
-}
-
-if [ $# -eq 0 ] || [ $# -eq 2 ]; then
-  SRC=adb
-  if [ "$1" == "-s" ]; then
-      ADB="adb -s $2"
-  else
-      ADB="adb"
-  fi
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
     exit 1
-  fi
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+    SRC=adb
+else
+    if [ $# -eq 1 ]; then
+        SRC=$1
+    else
+        echo "$0: bad number of arguments"
+        echo ""
+        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+        echo ""
+        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+        echo "the device using adb pull."
+        exit 1
+    fi
 fi
 
-BASE=../../../vendor/$VENDOR/g5-common/proprietary
-rm -rf $BASE/*
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
 
-DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVBASE/*
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
-extract ../../$VENDOR/g5-common/proprietary-files.txt $BASE
-extract ../../$VENDOR/$DEVICE/proprietary-files.txt $DEVBASE
+# Reinitialize the helper for device
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-./setup-makefiles.sh
+extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh
