c25af566e72324dbe1d818d86637c8cb5446b0ec
Rashed Abdel-Tawab <rashed@linux.com>
2016-09-11 21:20:20 -0400

    g5: Update GPS stack to LA.UM.5.5_rb1.10 and build api

--- g5.mk
+++ g5.mk
@@ -158,13 +158,16 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8996
+    gps.msm8996 \
+    libgnsspps \
+    libloc_api_v02 \
+    libloc_ds_api
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-		$(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
