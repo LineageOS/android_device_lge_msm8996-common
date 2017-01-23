ce81629dbcf234b3cb38ed9b9210271cac8d3041
Matt Filetto <matt.filetto@gmail.com>
2017-01-23 03:35:46 -0800

    msm8996: fix set_baseband.sh to allow spaces
    Change-Id: Ie7bf4b116dcffaf8501bc2e39f9fe4d005e3bc4a
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
 # grep the modem partition for baseband version and set it
-setprop gsm.version.baseband `strings /firmware/image/modem.b12 | grep "^MPSS.TH" | head -1`
+setprop gsm.version.baseband "`strings /firmware/image/modem.b12 | grep "^MPSS.TH" | head -1`"
