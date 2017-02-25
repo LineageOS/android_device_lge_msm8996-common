c70ca2d40ce779979ffecb24cb4f8d59fca2fb77
Rashed Abdel-Tawab <rashed@linux.com>
2017-02-24 22:43:52 -0500

    msm8996: Add IMS symlinks
    Change-Id: I35a5cd5e2327f82de44fc96934ca5457f28e0afc

--- Android.mk
+++ Android.mk
@@ -96,6 +96,16 @@ $(WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(WIDEVINE_SYMLINKS)
 # END WIDEVINE Images
 
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
 include device/lge/msm8996-common/tftp.mk
 
 endif
