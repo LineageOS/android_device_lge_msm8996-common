98e016aa0742d0fc10253599d026cea85634b7f7
Rashed Abdel-Tawab <rashed@linux.com>
2017-03-02 18:55:34 -0500

    msm8996: Don't inherit qcom/binaries
    Change-Id: I26af5792174bc9e4e46418b85a2a485e6d9bfa43

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -72,8 +72,6 @@ write_makefiles "$MY_DIR"/proprietary-files-qc-perf.txt
 echo "endif" >> "$PRODUCTMK"
 echo "endif" >> "$ANDROIDMK"
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8996/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
-
 # We are done with platform
 write_footers
 
