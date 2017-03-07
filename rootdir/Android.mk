38de51f77dc26903a382c5b183c29bcc9231c963
Rashed Abdel-Tawab <rashed@linux.com>
2017-03-07 01:53:51 -0500

    msm8996: Add init.power.sh to manage EAS and other PM
    * Remove wonky power setup in init.qcom.power.rc
    * Import init.power.sh from marlin
    * Add corresponding SELinux policy for init.power.sh
    
    Change-Id: Iaee177e20e6d2b71c5a939799f2023a7c8e5e33e

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,6 +12,13 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.power.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := bin/init.power.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
@@ -25,6 +32,7 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_REQUIRED_MODULES := init.power.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
