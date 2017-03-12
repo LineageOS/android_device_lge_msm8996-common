6d9b3093ff930ec0f766e3f5c138a354c7fcb27f
Rashed Abdel-Tawab <rashed@linux.com>
2017-03-12 19:01:31 -0400

    msm8996: Move power config into init.qcom.rc and add EAS'd perfd
    Half this power script is a grand mess anyway, so just import
    the parts used into init.qcom.rc and cleanup all the other power crap.
    Also add missing sepolicy for EAS enabled perfd
    
    Change-Id: I86563998aef207d5dc30fde116b37dd4a4717b55

--- msm8996.mk
+++ msm8996.mk
@@ -116,7 +116,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
-    init.qcom.power.rc \
     init.qcom.sh \
     init.qcom.sensors.sh \
     init.qcom.usb.rc \
