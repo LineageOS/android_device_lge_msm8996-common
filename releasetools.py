06489f6ecc19bf57244cf58643bbd7430d2ab8e7
dkati <dkatikaridis@gmail.com>
2017-03-17 03:29:40 +0200

    msm8996-common: Resize system filesystem on install
    * Most msm8996 variants have much larger system partitions.
    * Add a releasetools extension to resize /system
      at install time to allow use of bigger gapps
    
    Change-Id: Id7306f36f65b4fbe21bdac32114e868b81022209

--- releasetools.py
+++ releasetools.py
@@ -17,6 +17,11 @@ import hashlib
 import common
 import re
 
+def FullOTA_PostValidate(info):
+    info.script.AppendExtra('run_program("/tmp/install/bin/e2fsck_static", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+    info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+    info.script.AppendExtra('run_program("/tmp/install/bin/e2fsck_static", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+
 def FullOTA_Assertions(info):
   AddTrustZoneAssertion(info, info.input_zip)
   return
