f4ca089f306f96364a25240af6f7962aa6eef445
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-01 00:15:43 -0400

    msm8996: rootdir: Split off radio configs per-variant
    alice, elsa, and lucye all use different crap for their extra
    modem configurations and the main script was getting too messy.
    Just split this crap off into init.radio.sh per device.
    
    Change-Id: I8727b52bee4951d442bc7fd72c40ab9f562179c4

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -95,14 +95,6 @@ start_msm_irqbalance()
 	fi
 }
 
-start_copying_prebuilt_qcril_db()
-{
-    if [ -f /system/vendor/qcril.db -a ! -f /data/misc/radio/qcril.db ]; then
-        cp /system/vendor/qcril.db /data/misc/radio/qcril.db
-        chown -h radio.radio /data/misc/radio/qcril.db
-    fi
-}
-
 baseband=`getprop ro.baseband`
 echo 1 > /proc/sys/net/ipv6/conf/default/accept_ra_defrtr
 
@@ -317,25 +309,16 @@ case "$emmc_boot"
     ;;
 esac
 
-#
-# Copy qcril.db if needed for RIL
-#
-start_copying_prebuilt_qcril_db
-echo 1 > /data/misc/radio/db_check_done
-
-#
-# Make modem config folder and copy firmware config to that folder for RIL
-#
-rm -rf /data/misc/radio/modem_config
-mkdir /data/misc/radio/modem_config
-chmod 770 /data/misc/radio/modem_config
-if [ -d "/firmware/image/modem_pr/" ]; then
-  cp -r /firmware/image/modem_pr/mcfg/configs/* /data/misc/radio/modem_config
-else
-  cp -r /system/vendor/firmware/image/modem_pr/mcfg/configs/* /data/misc/radio/modem_config
-fi
-chown -hR radio.radio /data/misc/radio/modem_config
-echo 1 > /data/misc/radio/copy_complete
+contains() {
+    string="$1"
+    substring="$2"
+    if test "${string#*$substring}" != "$string"
+    then
+        return 1    # $substring is in $string
+    else
+        return 0    # $substring is not in $string
+    fi
+}
 
 #check build variant for printk logging
 #current default minimum boot-time-default
