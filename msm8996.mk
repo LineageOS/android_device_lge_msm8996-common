cdd90336ccdcdbd587572269a71a464295049648
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-02 19:54:16 -0400

    msm8996: Bring back hwaddrs
    So uh turns out that we have a bit of a MAC address issue...
    This reverts commits 5704c71bfab35fb9ea3c7b7f33e0ea26d108e691 and
    1b45f638de3f31f51bc9c0091552aca7cf1c6ef1
    
    Change-Id: I7efb93cb381eebac4f64d39084560aa0f33db7ea

--- msm8996.mk
+++ msm8996.mk
@@ -107,6 +107,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    hwaddrs
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
