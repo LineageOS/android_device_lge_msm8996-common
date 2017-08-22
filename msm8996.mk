a62bc16bc2736b158661f7c31044e75f5a6d9202
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:16:30 -0700

    msm8996: Move lineage specific overlays to overlay-lineage
    Change-Id: I370f6879026368a0ee5c1b90ebc6fcea3de37add

--- msm8996.mk
+++ msm8996.mk
@@ -20,6 +20,7 @@ $(call inherit-product-if-exists, vendor/lge/msm8996-common/msm8996-common-vendo
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
