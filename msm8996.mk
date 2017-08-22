8a01cdd6f06f23b4ac68987139fbc799c403cb08
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:43:30 -0700

    msm8996: Add keymaster and gatekeeper HAL impl and service
    Change-Id: I69d0ca034b7d27eea62d84d32fc643d6247de92e

--- msm8996.mk
+++ msm8996.mk
@@ -179,6 +179,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Gesture handler
 PRODUCT_PACKAGES += \
     GestureHandler
@@ -219,6 +224,11 @@ PRODUCT_COPY_FILES += \
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
