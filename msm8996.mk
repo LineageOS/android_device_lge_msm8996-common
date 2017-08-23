3b313891a99a90ed705a7d02048853c039f1a32c
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-23 08:35:15 -0700

    msm8996: Add mediaserver seccomp policies
    Change-Id: I70ab0cd61e8bf8bceb9553534157e03178ea89af

--- msm8996.mk
+++ msm8996.mk
@@ -304,6 +304,11 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librmnetctl
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
