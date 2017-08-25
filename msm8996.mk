2d6ed48634f8492e81a058e858809f9d92498348
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-24 20:57:50 -0700

    msm8996: Add filter_ie for WiFi
    Change-Id: I83b9e0914154252434854493766123e89db1cd3c

--- msm8996.mk
+++ msm8996.mk
@@ -351,4 +351,5 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/filter_ie:system/etc/wifi/filter_ie \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
