7961ade7da9cf989efa126c1d16e131c36be1d31
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-06 18:53:46 -0700

    msm8996: Build libhypv_intercept
    Change-Id: Iffaa45aeb3d8d95aa7d07fd46df30abb96b94fb5

--- msm8996.mk
+++ msm8996.mk
@@ -233,6 +233,7 @@ PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libdashplayer \
     libdivxdrmdecrypt \
+    libhypv_intercept \
     libmm-omxcore \
     libextmedia_jni \
     libOmxAacEnc \
