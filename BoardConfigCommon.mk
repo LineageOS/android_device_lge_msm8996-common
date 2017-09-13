f005a8dc4fec9bc51d43adedd7f7ba7ff68202e9
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 16:15:29 -0700

    msm8996: Remove /system from fstab.qcom and add fstab.full
    System is mounted by the kernel now, no need for a duplicate mount.
    However, the OTA fails to compile if there is no /system in the fstab
    so add fstab.full to allow the build to finish
    
    Change-Id: Id8720740c1a1844021da2678d7214d2b321483c7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,7 +179,7 @@ BOARD_USES_QC_TIME_SERVICES := true
 PRODUCT_BOOT_JARS += tcmiface tcmclient com.qti.dpmframework dpmapi com.qti.location.sdk
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.full
 
 # RIL
 TARGET_RIL_VARIANT := caf
