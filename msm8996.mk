a6b7966e2e146e1157ba3403c5cfbc46e5c73654
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-24 21:28:37 -0700

    msm8996: Add DisplayEventReceiver and GraphicBuffer shim for camera
    These functions changed between N and O so forward the old functions to
    their new counterparts.
    
    Change-Id: Id66dd0cc5c37e09f576b9502e7498a2f63fa04f5

--- msm8996.mk
+++ msm8996.mk
@@ -125,6 +125,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    libshim_camera \
     Snap
 
 # Charger
