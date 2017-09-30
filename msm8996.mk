f3642554915a8e7d0fd1c4f24c36eb01f4c3970e
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 17:22:01 -0400

    msm8996: Build lights HIDL interface
    Change-Id: I22a6b26f01c242dba2b83ef267558747be1f8c6c

--- msm8996.mk
+++ msm8996.mk
@@ -211,6 +211,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
     lights.msm8996
 
 # LiveDisplay native
