b0dcbcc74f04d2b2858bfab97a8d6518a79c4fd2
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 13:06:58 -0400

    msm8996: Built libbt-vendor
    Change-Id: I1ef68c289377c04429226522b82344809e52f441

--- msm8996.mk
+++ msm8996.mk
@@ -118,7 +118,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
-    hwaddrs
+    hwaddrs \
+    libbt-vendor
 
 # Camera
 PRODUCT_PACKAGES += \
