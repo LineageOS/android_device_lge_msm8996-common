d1887f4e44ed4389e93f36b97cf37fdb7c5e2ddf
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 12:56:21 -0400

    msm8996: Add fingerprint service to manifest
    This is currently decommonized since some models need the 2.1 service while
    other need the 2.0 service, so the service is built in [g5,g6,v20]-common.
    Also remove references to fingerprintd
    
    Change-Id: I358797bb813856db074c8aa0b6c9ea24cb4c49fe

--- msm8996.mk
+++ msm8996.mk
@@ -171,10 +171,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    fingerprintd
-
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
