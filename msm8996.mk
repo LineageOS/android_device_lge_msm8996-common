2fa39a73c71df75425c7d2de29afe7b97794f1d0
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 13:08:15 -0400

    msm8996: Build NFC service
    Change-Id: I72bfb700e3d9b2e2c9b3c482413642624f962b1c

--- msm8996.mk
+++ msm8996.mk
@@ -252,6 +252,8 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service \
     com.android.nfc_extras \
     nfc_nci.msm8996 \
     NfcNci \
