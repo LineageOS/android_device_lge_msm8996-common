e2d3b504300919d7714af12e573ae4096ae6b11b
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-01 00:25:27 -0400

    msm8996: Add forgotten bluetooth HIDL HAL
    Change-Id: I3979c3f2e27d7dab6684301bf27ac6c31bb04dd7

--- msm8996.mk
+++ msm8996.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
     hwaddrs \
     libbt-vendor
