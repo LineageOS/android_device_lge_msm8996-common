ab814ab2843d541858f3b3f8370eeb26bde2513f
Glen Kuhne <kuh@google.com>
2017-10-02 09:46:59 -0700

    msm8996: Add p2p_no_group_iface=1 to p2p_supplicant_overlay
    * Configures wpa_supplicant to use p2p0 mgmt iface instead of dynamically
      created p2p-p2p0-XYZ virtual interfaces for P2P group.
    
    Bug: 65524423
    Test: CtsVerifier WifiDirect
    Test: Manual verification
    Change-Id: I70582f114ab69684424aac41fbeb78fa7cc04847
    Merged-In: Id9add59aa122adb7f39c80252061a8f5d3cc55cd

--- msm8996.mk
+++ msm8996.mk
@@ -375,4 +375,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/filter_ie:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/filter_ie \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
