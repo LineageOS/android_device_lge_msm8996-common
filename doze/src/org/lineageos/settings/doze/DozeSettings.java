f8e6eec4bba2706d315a77048785385f4083b9aa
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-09 00:51:41 -0400

    msm8996: Update doze package for oreo
    Change-Id: I3babc0825224eaae8066f656d5280cfc7c67e5cb

--- doze/src/org/lineageos/settings/doze/DozeSettings.java
+++ doze/src/org/lineageos/settings/doze/DozeSettings.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cyanogenmod.settings.doze;
+package org.lineageos.settings.doze;
 
 import android.app.Activity;
 import android.os.Bundle;
