e9014f31e1fc2d632f6b39a36a8fa9e32ab4ee03
Elliott Mitchell <ehem+android_kernel_lge_msm8996@m5p.com>
2017-10-20 15:53:31 -0700

    msm8996: Add h990 to build guard
    Making LineageOS fully operational on the H990 shouldn't take too long
    so start getting patches into Gerrit.
    
    Sort the device lists based on their numeric values, ignoring leading
    alphabetic characters.
    
    Change-Id: Iabf6fa4907e998310f43f42733f812778b41c7c3

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -20,7 +20,7 @@ set -e
 
 export INITIAL_COPYRIGHT_YEAR=2016
 export G5_DEVICE_LIST="g5 h830 h850 rs988"
-export V20_DEVICE_LIST="v20 h910 h918 us996 ls997 vs995"
+export V20_DEVICE_LIST="v20 h910 h918 h990 vs995 us996 ls997"
 export G6_DEVICE_LIST="g6 h870 h872 us997"
 
 # Load extract_utils and do some sanity checks
