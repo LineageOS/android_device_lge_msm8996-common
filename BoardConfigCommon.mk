62d3975a3d5ea39c6aabb06cd4a3110c425e9218
NateM <nathan90@gmail.com>
2018-03-25 11:06:32 -0400

    msm8996: Remove offmode charging flags and fix offline charger service
    This is handled in kernel now
    
    Change-Id: Ice98347cd4cc3da073d072b1e427eaa11ec1b9c3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,9 +115,6 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_CHARGER_ENABLE_SUSPEND := false
 BACKLIGHT_PATH := "/sys/class/leds/lcd-backlight/brightness"
 
-BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
-BOARD_CHARGING_CMDLINE_VALUE := "chargerlogo"
-
 # Camera
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/hw/camera.msm8996.so|/system/vendor/lib/libshim_camera.so \
