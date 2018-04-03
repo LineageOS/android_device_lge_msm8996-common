f6425a3f37679f41063d07f682f4826ca76dc8aa
Eric Meddaugh <eric@meddaughs.com>
2018-04-03 09:43:08 -0400

    msm8996: Enabled bluetooth WBS
    Change-Id: Ifc5840c0ff00fe9f6dd6e2171712fe6f8a5c5e09

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -39,6 +39,9 @@ inline const char* BtmGetDefaultName()
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
+#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
+
 #define BLE_VND_INCLUDED  TRUE
 
 #endif
