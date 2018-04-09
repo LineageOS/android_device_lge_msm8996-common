1d1ee05c8295aa8e19cafe78443748c245b10dbe
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-08 17:03:34 -0700

    msm8996: Build a system version of android.hidl.base@1.0 for imsvt
    Change-Id: Icb911f414595117a28fb764e145fbbf6a7ee0787

--- msm8996.mk
+++ msm8996.mk
@@ -191,6 +191,7 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
