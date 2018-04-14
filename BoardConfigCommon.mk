7894db0cdf60859baadbeeda082e1999ea970df3
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-14 11:40:22 -0700

    msm8996: Get rid of duplicate fstab and just use recoveryonly
    Change-Id: I4b01b814cb7b9306b757753925e0c11445a80654

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,7 +192,7 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.full
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux policies
 include device/qcom/sepolicy/sepolicy.mk
