d201d3e13f6e3b9dd3bfacbe9955cead32c58a62
Eric Meddaugh <eric@meddaughs.com>
2018-04-17 10:16:24 -0400

    [DNM]msm8996: Add loggy and enabled permissive
    * This is for testing/debug only.
    
    Change-Id: I135e296ba414ec7b6a4456c1388aef333274fbdc

--- msm8996.mk
+++ msm8996.mk
@@ -380,3 +380,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/filter_ie:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/filter_ie \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+#
+# loggy
+PRODUCT_PACKAGES += \
+    loggy.sh
