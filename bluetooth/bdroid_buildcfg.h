7041ef31d84b533168e320d149781b7ba405ae79
Hansong Zhang <hsz@google.com>
2018-04-17 16:11:03 -0700

    msm8996-common: Bluetooth HFP: Use WBS by default
    Bug: 72624793
    Test: compilation
    Change-Id: I78ef8705d9fd38db10a4d8f2bc0eac58bcf61d0a

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -42,8 +42,8 @@ inline const char* BtmGetDefaultName()
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #define BLE_VND_INCLUDED  TRUE
-#define BTM_WBS_INCLUDED TRUE /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED TRUE /* Use WBS */
+
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 #pragma pop_macro("PROPERTY_VALUE_MAX")
 
