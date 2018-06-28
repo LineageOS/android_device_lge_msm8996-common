03eefab87c92af531d43f50587ce42e91ce650ab
Rashed Abdel-Tawab <rashed@linux.com>
2018-06-27 19:28:28 -0700

    msm8996: Move wpa_supplicant_overlay.conf to vendor
    Change-Id: I2c418980414f66a79c7d3b0db85f227fa7829ae9

--- msm8996.mk
+++ msm8996.mk
@@ -366,5 +366,5 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wifi/filter_ie:system/etc/wifi/filter_ie \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+    $(LOCAL_PATH)/configs/wifi/filter_ie:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/filter_ie \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
