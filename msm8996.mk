0bca1d02f3932cb1075fcb3cb9f5386e38ba509a
Rashed Abdel-Tawab <rashed@linux.com>
2018-06-27 19:27:37 -0700

    msm8996: Build vendor version of libstdc++
    Change-Id: Ib834f3e315ae6504469c0eee7b3fe13a1c1a737f

--- msm8996.mk
+++ msm8996.mk
@@ -354,6 +354,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libstdc++.vendor \
+    vndk_package
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
