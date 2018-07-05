cd3178e5e8f88994b0fb6a66b62e218311d55e9e
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-04 20:37:58 -0700

    msm8996: Remove legacy lights HAL
    alice, elsa and lucye will each have their own binderized HAL.
    Also remove the blink pattern sysfs nodes and move to alice
    
    Change-Id: Iaa0f0f748f334cc2b352318ab9793fbe151bc4c8

--- msm8996.mk
+++ msm8996.mk
@@ -218,12 +218,6 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8996
-
 # LiveDisplay native
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@1.0-service-sdm
