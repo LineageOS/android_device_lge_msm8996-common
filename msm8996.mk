94514a5f2895d052d71c32356db00fa489262b54
Eric Meddaugh <eric@meddaughs.com>
2018-07-13 07:48:52 -0400

    msm8996: Revert back to stock thermal
    * Decommonize the thermal-engine-8996.conf
    
    Change-Id: I5907ce776e4bacf1259b7b761728a59289cf14b5

--- msm8996.mk
+++ msm8996.mk
@@ -319,11 +319,12 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-default.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-default.conf
 
 # Tools
 PRODUCT_PACKAGES += \
