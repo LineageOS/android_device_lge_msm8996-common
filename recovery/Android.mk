5044d55f7f7924ae583ed6ed5e3f46875baf4b1b
Eric Meddaugh <eric@meddaughs.com>
2018-07-25 15:13:04 -0400

    msm8996: releasetools: Add trustzone assert
    * Allow for minimum trustzone assert version
    
    Change-Id: Ib95cf02bfc121be2f6c0c907f2c2ce1e3ed6901b

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,7 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := bootable/recovery
+
+ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
+LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
+endif
+
+LOCAL_C_INCLUDES := \
+    $(call project-path-for,recovery) \
+    $(call project-path-for,recovery)/updater/include \
+    bootable/recovery \
+    system/core/base/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_msm8996
 LOCAL_MODULE_TAGS := eng
