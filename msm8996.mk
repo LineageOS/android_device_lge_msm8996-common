26b7585ac35170a6a02a34063a4e67d70655bf06
Ethan Chen <intervigil@gmail.com>
2019-02-28 22:16:46 -0800

    msm8996: Convert bluetooth@1.0-impl to read QC MAC address
    * hwaddrs cannot be used anymore since ro.boot.btmacaddr is not
      writable by vendor domains. bluetooth_prop is off-limits to vendor
      domains now.
    * Have the Bluetooth HAL impl directly read
      /persist/.bdaddr itself and pull the MAC address out.
    
    Change-Id: If4b2b99345eb470c140523906a56299ec5decc9a

--- msm8996.mk
+++ msm8996.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-impl-lge.msm8996 \
     android.hardware.bluetooth@1.0-service \
     hwaddrs \
     libbt-vendor
