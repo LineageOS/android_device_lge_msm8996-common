f6949580e7ab242371fa35330150b176746f3c2c
Wang Han <416810799@qq.com>
2019-03-04 19:15:19 +0800

    msm8996-common: Remove com.android.future.usb.accessory from PRODUCT_PACKAGES
     * It is in core-minimal.mk.
    
    Change-Id: I2fff428fed504dedd0d3aa71ee80018a0880275e

--- msm8996.mk
+++ msm8996.mk
@@ -377,8 +377,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
-    com.android.future.usb.accessory
+    android.hardware.usb@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
