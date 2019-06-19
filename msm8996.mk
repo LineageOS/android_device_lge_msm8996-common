15af34e98f55bee659c24b27dccdc1a73550dfda
Eric Meddaugh <eric@meddaughs.com>
2019-06-19 14:24:42 -0400

    msm8996-common: Update audio from LA.UM.7.5.r1-03100-8x96.0
    Change-Id: I8179655632eb21d0d4a1a01a5c600cf505130f77

--- msm8996.mk
+++ msm8996.mk
@@ -82,6 +82,12 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.0-service \
+    android.hardware.audio@4.0 \
+    android.hardware.audio.common@4.0 \
+    android.hardware.audio.common@4.0-util \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0 \
+    android.hardware.audio.effect@4.0-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8996 \
