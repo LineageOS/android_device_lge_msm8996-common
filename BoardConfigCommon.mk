7b301ddc35c35ba3400c91846f1fc1a8d6f33aae
Nolen Johnson <johnsonnolen@gmail.com>
2019-09-08 03:58:47 +0000

    hax
    Change-Id: Ia8024b1995668e0e00566dcb4dd3198c72beb9e4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,6 +182,10 @@ BOARD_USES_QC_TIME_SERVICES := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux policies
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
