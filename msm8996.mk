847a7e57953f68a2d6f79c1e284ddc2bc6db94f2
Eric Meddaugh <eric@meddaughs.com>
2019-09-08 16:35:56 -0400

    msm8996-common: Add qcom privapp permissions
    * https://source.android.com/devices/tech/config/perms-whitelist#generating-whitelists
    
    Change-Id: I319105460e55b562ac3bcdd95e01d30b2264a042

--- msm8996.mk
+++ msm8996.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # IPACM
 PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
