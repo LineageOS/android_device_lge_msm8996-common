08aa6359664715e9d140b07024a229829fc248f1
ROMSG <48342780+ROMSG@users.noreply.github.com>
2019-11-25 09:08:16 -0800

    re-add android.mk

---
+++ recovery/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
+LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
+endif
+
+LOCAL_C_INCLUDES := \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include \
+    bootable/recovery/updater/include \
+    system/core/base/include
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_msm8996
+include $(BUILD_STATIC_LIBRARY)
