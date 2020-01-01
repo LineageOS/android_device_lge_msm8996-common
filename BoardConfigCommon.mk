f6930d032c662797d05c3ca49fbdaa62325dd8d8
Daniel Schreiber <daniel+lineage@schreiberseits.de>
2020-01-01 20:56:25 +0100

    msm8996-common: sepolicy neverallow fixes
    note that hwaddrs needs to change its selinux domain to work around the
    neverallow rules
    
    Change-Id: I9dbd04802db166017b970bc111e1b9ef09d08f6d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -189,7 +189,6 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 # SELinux policies
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
