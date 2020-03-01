7ab5791a85c356baf4e753a4c0c8a95617f44ad4
Ethan Chen <intervigil@gmail.com>
2020-02-29 18:15:48 -0800

    msm8996-common: Convert HIDL compat hacks to Blueprint
    * [npjohnson] system version not needed/not available anymore.
    
    Change-Id: I612f96afc725d35b22b0f337df4c8e5cc7bbaf79

--- msm8996.mk
+++ msm8996.mk
@@ -217,7 +217,6 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     libhidltransport \
     libhwbinder
