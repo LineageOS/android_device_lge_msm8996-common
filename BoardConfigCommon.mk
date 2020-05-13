4b4eb45096eca001e16af062eb9172716164c72b
Roger Ellis <ellisr@google.com>
2020-05-13 16:56:10 -0700

    msm8996-common: Allow ELF prebuilts in PRODUCT_COPY_FILES.
     * ELF prebuilts were banned in PRODUCT_COPY_FILES starting
       May 11th. See b/156534160 for long-term fix which should
       override this change.
    
    Change-Id: Icd25b40ca4e4285319775046a1b70de30572be07

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,6 +20,9 @@ COMMON_PATH := device/lge/msm8996-common
 # Inherit from the proprietary version
 include vendor/lge/msm8996-common/BoardConfigVendor.mk
 
+# BUILD_BROKEN_*
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
