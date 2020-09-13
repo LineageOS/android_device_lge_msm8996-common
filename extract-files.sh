60f5bdbd2a4efc4fcb588548e56a202abf7606e1
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:18:28 +0800

    msm8996-common: Add prebuilt protobuf from sdk29
    This is needed for our prebuilt DRM library.
    
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- extract-files.sh
+++ extract-files.sh
@@ -29,6 +29,9 @@ function blob_fixup() {
     vendor/lib64/libsettings.so)
         patchelf --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v28.so" "${2}"
         ;;
+    vendor/lib64/libwvhidl.so)
+        "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
+        ;;
     esac
 }
 
--- msm8996.mk
+++ msm8996.mk
@@ -348,7 +348,8 @@ PRODUCT_PACKAGES += \
     vndk-sp \
 
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v28.so
+    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v28.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
 
 # Telephony
 PRODUCT_PACKAGES += \
