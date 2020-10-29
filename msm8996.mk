171b6aa361fb8eb95538a7f8091b28bd3ca469c5
Alessandro Astone <ales.astone@gmail.com>
2020-10-29 08:40:54 +0100

    msm8996-common: Define a soong namespace
    Change-Id: Ida759ee1786c667e766da29103f0ecb54950c104

--- msm8996.mk
+++ msm8996.mk
@@ -346,6 +346,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # TimeKeep
 PRODUCT_PACKAGES += \
     timekeep \
