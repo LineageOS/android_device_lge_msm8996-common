cb0d4209dfefabb86c1f16eab2200c69bc0af083
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    msm8996-common: Switch to standalone extract utils
    Somewhat align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-export INITIAL_COPYRIGHT_YEAR=2016
 export G5_DEVICE_LIST="g5 h830 h850 rs988"
 export V20_DEVICE_LIST="v20 h910 h915 h918 h990 vs995 us996 ls997"
 export G6_DEVICE_LIST="g6 h870 h872 us997"
@@ -27,9 +16,9 @@ export G6_DEVICE_LIST="g6 h870 h872 us997"
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -37,7 +26,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common platform
-setup_vendor "$PLATFORM_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "$PLATFORM_COMMON" "$VENDOR" "$ANDROID_ROOT" true
 
 # Copyright headers and common guards
 BKP_DEVICE_COMMON="$DEVICE_COMMON"
@@ -53,7 +42,7 @@ write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
 write_footers
 
 # Reinitialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$ANDROID_ROOT" true
 
 # Copyright headers and guards
 case "$DEVICE_COMMON" in
@@ -78,7 +67,7 @@ write_makefiles "$MY_DIR/../$DEVICE_COMMON/proprietary-files.txt" true
 write_footers
 
 # Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$ANDROID_ROOT"
 
 # Copyright headers and guards
 write_headers
