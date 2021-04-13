7964fe6f325b83bcd821c55384919a79139b3768
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    msm8996-common: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- msm8996.mk
+++ msm8996.mk
@@ -39,6 +39,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service.lge_msm8996 \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
