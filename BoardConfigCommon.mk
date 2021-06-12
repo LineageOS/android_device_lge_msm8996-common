a73196e01e69f1b40cdc4f8879c5f1f393fa39e8
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-06-12 19:16:12 +0200

    msm8996-common: skip new camera kind check
    opting out of new camera kind check introduced in
    Ic849aa91e4d953f45ed99efae997e45671d116b3
    
    Change-Id: I972544da22ab21197cd73a4b8caa4011868a6f1d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,6 +112,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/hw/camera.msm8996.so|/system/vendor/lib/libshim_camera.so \
     /system/vendor/lib/libmmcamera_ppeiscore.so|/system/vendor/lib/libshim_camera.so
+TARGET_CAMERA_SKIP_KIND_CHECK := true
 USE_CAMERA_STUB := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
