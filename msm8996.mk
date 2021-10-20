5ded154e9403cbde9487b0196922cea94880f2e3
Erfan Abdi <erfangplus@gmail.com>
2021-10-20 17:20:19 +0300

    msm8996-common: ims: Add shim for camera blobs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I5e622e68422f5c4da13c0752a7eb3f4b20ddae49

--- msm8996.mk
+++ msm8996.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    libgui_shim \
     libshim_camera \
     libtinyxml \
     vendor.qti.hardware.camera.device@1.0
