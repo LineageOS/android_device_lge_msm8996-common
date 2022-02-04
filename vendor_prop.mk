892e7836a766d0db61d22fb0aa69cfcdc62da134
Sal Savage <salsavage@google.com>
2022-02-04 09:41:57 -0800

    msm8996-common: Enable bluetooth profiles with product.prop
    Bug: 217448211
    Test: make -j
    Change-Id: Idd7e52b7822915ed61299f1c0b29bf87ca0339fe

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -39,6 +39,29 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.spkr.cal.duration=5
 
 # Bluetooth
+PRODUCT_PRODUCT_OVERRIDES += \
+    bluetooth.profile.asha.central.enabled=true \
+    bluetooth.profile.a2dp.source.enabled=true \
+    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.bap.broadcast.assist.enabled=true \
+    bluetooth.profile.bap.unicast.server.enabled=true \
+    bluetooth.profile.bas.client.enabled=true \
+    bluetooth.profile.csip.set_coordinator.enabled=true \
+    bluetooth.profile.gatt.enabled=true \
+    bluetooth.profile.hap.client.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=true \
+    bluetooth.profile.hid.device.enabled=true \
+    bluetooth.profile.hid.host.enabled=true \
+    bluetooth.profile.map.server.enabled=true \
+    bluetooth.profile.mcp.server.enabled=true \
+    bluetooth.profile.opp.enabled=true \
+    bluetooth.profile.pan.nap.enabled=true \
+    bluetooth.profile.pan.panu.enabled=true \
+    bluetooth.profile.pbap.server.enabled=true \
+    bluetooth.profile.sap.server.enabled=true \
+    bluetooth.profile.tbs.server.enabled=true \
+    bluetooth.profile.vc.server.enabled=true
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bt.bdaddr_path=/data/misc/bluetooth/bdaddr \
     ro.rfkilldisabled=1
