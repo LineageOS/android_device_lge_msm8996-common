1d9fc8fd1b5eb0616fac9d130c5bd45d82f0b4a5
xxseva44 <dev.xxseva44@gmail.com>
2023-01-15 16:50:06 -0500

    msm8996-common: Add support for LG BRCM V4L2 driver
    * On 4.4, we have ditched the old Bluetooth driver and
      have gone to the BCRM V4L2 driver.
    
    This is a squash of the following commits:
    a12725d05c27d83b84aa10b9c3a3b1624d8ef974
    fe79f9eb33b8ce47f6841e4d4d5f4b8482e073f0
    8f0ac8d7840aeac5f8dd506a755c515235795235
    865ea6a34b218b900eaf6b4d44049b7d06063eaf
    8d5f6c072f5d3452679bf2f26e116eccabe19ac0
    c78c26ffcf707656f51d6fac56613041f296027a
    
    Change-Id: I99427a5f6ca3ba4a382c5f79f44ff50c38aa5155

--- msm8996.mk
+++ msm8996.mk
@@ -79,6 +79,9 @@ PRODUCT_PACKAGES += \
     libbt-vendor
 
 PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
@@ -152,6 +155,12 @@ PRODUCT_COPY_FILES += \
 # FlipFlap
 PRODUCT_PACKAGES += \
     FlipFlap
+
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    brcm-uim-sysfs \
+    libfmjni
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
