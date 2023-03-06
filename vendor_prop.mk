e9d7c56c2f075c0f7996a63e3f65dd265e176212
Nolen Johnson <johnsonnolen@gmail.com>
2023-03-06 11:50:32 -0500

    msm8996-common: properties: Disable SDM Scalar
    * Else, ScalarConfig: :GetPipeScaleSettingsInfo: lib_scale_get_pipe_settings failed: status = 2
    Change-Id: I3c79a7cc561e6ba4c940bd418ae04921a795081c
    
    Change-Id: I7ee49c90f7ad948641d81b3c261351a6b3728fed

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -80,7 +80,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.cabl=0 \
     ro.sf.lcd_density=560 \
-    debug.stagefright.ccodec=0
+    debug.stagefright.ccodec=0 \
+    vendor.display.disable_scaler=1
 
 # Factory Reset Protection (FRP)
 PRODUCT_PROPERTY_OVERRIDES += \
