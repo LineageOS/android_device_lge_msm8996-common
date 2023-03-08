f554c5ab402ff7be6ef62291b965ccd19e93d32d
Nolen Johnson <johnsonnolen@gmail.com>
2023-03-08 11:38:24 -0500

    msm8996-common: librecovery_updater: Unguard boot-device cflag
    Change-Id: Iefcbabcf6b7f9ca3a7a506ab08549a35bc56a4ad

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,9 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
 LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
-endif
 
 LOCAL_C_INCLUDES := \
     bootable/recovery/edify/include \
