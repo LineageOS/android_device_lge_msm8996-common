0eba4137ae14c318283cde1cee766462c529aec6
Nolen Johnson <johnsonnolen@gmail.com>
2023-03-08 11:16:08 -0500

    msm8996-common: Drop QCOM WFD dependencies
    Change-Id: I21debe0dbeedf03cb71eec26a48bb1a24d8dc4bf

--- msm8996.mk
+++ msm8996.mk
@@ -419,7 +419,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
-
-# Wi-Fi Display
-PRODUCT_PACKAGES += \
-    libnl
