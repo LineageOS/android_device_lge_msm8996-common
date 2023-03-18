LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: ims: Make shim for libui
P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel = nullptr, int32_t* outBytesPerStride = nullptr);
Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv"

Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee

extract-files.sh
@@ -44,6 +44,11 @@ function blob_fixup() {
             "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
         done
         ;;
+    system_ext/lib64/lib-imsvideocodec.so)
+        for LIBUI_SHIM in $(grep -L "libui_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libui_shim.so" "${LIBUI_SHIM}"
+        done
+        ;;
     system_ext/lib64/lib-imsvt.so)
         for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
             "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"

LineageOS/android_device_lge_msm8996-common

extract-files.sh
@@ -26,8 +26,14 @@ function blob_fixup() {
     system/lib/lib-imscamera.so)
         "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
         ;;
+    system/lib/lib-imsvideocodec.so)
+        "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+        ;;
     system/lib64/lib-imscamera.so)
         "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+        ;;
+    system/lib64/lib-imsvideocodec.so)
+        "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
         ;;
     system/lib64/lib-imsvt.so)
         "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"