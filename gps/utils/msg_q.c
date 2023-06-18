de062d1be725c93df8973e02331615458c3ed880
AShiningRay <genesis45.ppc@gmail.com>
2023-06-17 20:05:49 -0400

    msm8996-common: gps: utils: Fix some warnings that will soon be errors
    msg_q was trying to use a linked_list enum instead of its own, and
    LocIpc had an unused parameter in its getRecver() function.
    
    Change-Id: I3e01d28bbaf9c4ed8d60d8e2867df1122a7a5385

--- gps/utils/msg_q.c
+++ gps/utils/msg_q.c
@@ -296,7 +296,7 @@ msq_q_err_type msg_q_rmv(void* msg_q_data, void** msg_obj)
    if (linked_list_empty(p_msg_q->msg_list)) {
       LOC_LOGW("%s: list is empty !!\n", __FUNCTION__);
       pthread_mutex_unlock(&p_msg_q->list_mutex);
-      return eLINKED_LIST_EMPTY;
+      return eMSG_Q_EMPTY;
    }
 
    rv = convert_linked_list_err_type(linked_list_remove(p_msg_q->msg_list, msg_obj));
