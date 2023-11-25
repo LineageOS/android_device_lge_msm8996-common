62f1ff5809c9da846c9bdc4b2e7b411c5d6d6835
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-24 21:36:38 -0500

    msm8996-common: Drop VNDK-SP
    * Useless, if anything, we will go with VNDK current later
      once we resolve outstanding issues.
    
    Change-Id: I702427ecfdde2cabb624e86dd5ead7aa50ab8b30

--- msm8996.mk
+++ msm8996.mk
@@ -408,9 +408,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VNDK
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so \
