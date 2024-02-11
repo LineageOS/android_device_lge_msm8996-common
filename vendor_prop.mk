0be41535a7274804ec2286620dc032f3c5506117
AShiningRay <genesis45.ppc@gmail.com>
2024-02-11 15:52:33 -0300

    msm8996-common: Tune LMKD parameters to improve multitasking.
    Although we have moved from the in-kernel lmk to lmkd, we didn't
    tune it to account for this platform's memory layout. The end
    result was an overly aggressive killing strategy that rarely let
    more than one application run at any given time, sometimes even
    killing tasks in the foreground if they were deemed too intensive,
    with "intensive" being anything that pushed those devices beyond
    65-70% RAM usage... on a system that already hovers around 50% usage
    by default.
    
    These tweaks should help this platform multitask a lot better,
    letting it use almost all of its RAM as well as a decent portion of
    the allocated ZRAM swap space before deciding to kill applications.
    
    Change-Id: Idbd0902e263672aedb7e18343038a06fb9b288fa

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -151,13 +151,20 @@ PRODUCT_PRODUCT_OVERRIDES += \
 
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.low_ram=false \
+    ro.lmk.use_psi=false \
     ro.lmk.low=1001 \
     ro.lmk.medium=800 \
     ro.lmk.critical=0 \
     ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true
+    ro.lmk.upgrade_pressure=87 \
+    ro.lmk.downgrade_pressure=75 \
+    ro.lmk.kill_heaviest_task=false \
+    ro.lmk.kill_timeout_ms=50 \
+    ro.lmk.thrashing_limit=50 \
+    ro.lmk.thrashing_limit_decay=40 \
+    ro.lmk.swap_free_low_percentage=15 \
+    ro.lmk.swap_util_max=90
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
