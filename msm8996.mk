b8643ed35b0803fd947bab837367a6895017c8b2
Bruno Martins <bgcngm@gmail.com>
2024-03-31 22:36:51 +0100

    msm8996-common: Bringup telephony after U QPR2
    Change-Id: I10e23a3982ddeee4ddbbe03509cba704410b52bc

--- msm8996.mk
+++ msm8996.mk
@@ -89,8 +89,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.4:64 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5:64 \
     camera.device@3.2-impl \
     libexif_32 \
     libshim_camera \
@@ -319,9 +321,13 @@ PRODUCT_COPY_FILES += \
 include $(LOCAL_PATH)/vendor_prop.mk
 
 # Qualcomm
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/permissions/system-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
 # Recovery
 PRODUCT_PACKAGES += \
@@ -335,7 +341,10 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libqti_vndfwk_detect \
     librmnetctl \
-    android.hardware.radio@1.4-service.legacy \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.0.vendor \
     libnetutils.vendor \
     libsqlite.vendor \
     libsysutils.vendor
@@ -377,6 +386,10 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
