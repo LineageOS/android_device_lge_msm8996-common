a1fc261987f9e6ee5514164513a18bea17a5514f
Nolen Johnson <johnsonnolen@gmail.com>
2024-06-03 18:30:17 -0400

    Revert "Revert "Revert "msm8996-common: Disable compressed APEX"""
    This reverts commit 359c7b7d5fcf797ad1823aae40cf39470e1ee517.
    
    Change-Id: Id441b330ee7906311ae7310bb6bc6b817fc373b1

--- msm8996.mk
+++ msm8996.mk
@@ -24,12 +24,6 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 560dpi
 
-# APEX
-# Disable APEX compression
-# Keep this after including updatable_apex.mk
-PRODUCT_COMPRESSED_APEX := false
-
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
