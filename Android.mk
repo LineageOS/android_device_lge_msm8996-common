e417fbdf8599c6b39feeff3ce6d5eca7f032e9ab
Han Sol Jin <hansol@hansol.ca>
2024-06-25 21:34:28 -0700

    msm8996-common: update fstab from H87030b
    lucye_global_com-user 9 PKQ1.190522.001 1930916292597 release-keys
    - Add minimal changes from stock fstab
    - Add /vendor/firwmare_mnt mount point for modem
    - Switch from BOARD_ROOT_EXTRA_FOLDERS to BOARD_ROOT_EXTRA_SYMLINKS to
      keep compatibility with G5
      - TODO: do any of the G5 blobs still need the root directories?
    - Add init changes to accomodate for new mount points of drm,mpt,sns
    
    Change-Id: I80be4200f82a4f790cf3d11b7cc03f22221b43fe

--- Android.mk
+++ Android.mk
@@ -23,6 +23,15 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
+# A/B builds require us to create the mount points at compile time.
+# Just creating it for all cases since it does not hurt.
+FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
+$(FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT)
+
 # CPPF Images
 CPPF_IMAGES := \
     cppf.b00 cppf.b01 cppf.b02 cppf.b03 cppf.b04 \
