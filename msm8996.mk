365926b319b064f31fbf934567bc7c2e08f034e7
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    msm8996-common: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- msm8996.mk
+++ msm8996.mk
@@ -134,14 +134,12 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.msm8996 \
     hdmi_cec.msm8996 \
     hwcomposer.msm8996 \
     libdisplayconfig \
     libqdMetaData.system \
-    memtrack.msm8996
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
