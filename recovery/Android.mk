575f2368eec274aa814168504a608793add99adc
Christian Hoffmann <chrmhoffmann@gmail.com>
2024-09-29 18:39:07 +0200

    msm8996-common: Align with new recovery structure
    Change-Id: Ic3ecf63a2888aac08aeaf40677ae7bfa74912030

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 
 LOCAL_C_INCLUDES := \
-    bootable/recovery/edify/include \
+    bootable/deprecated-ota/edify/include \
     bootable/recovery/otautil/include \
     bootable/recovery/updater/include \
     system/libbase/include
