af1b6408638bbbe39d75c9fe2c971f156d277e08
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-28 01:51:44 -0500

    msm8996-common: Use patchelf 0.18 everywhere
    * On bash extract, 0.9 is default, whereas on py extract, 0.18 is.
    * Correct libwvdrmengine.so origin hash while here.
    
    Change-Id: I37589d5b1bc5fdaa246b1ba892f0ecb5a9aa50e7

--- extract-files.sh
+++ extract-files.sh
@@ -53,7 +53,7 @@ function blob_fixup() {
         "${PATCHELF_0_18}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
         ;;
     vendor/bin/pm-service)
-        grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
+        grep -q libutils-v33.so "${2}" || "${PATCHELF_0_18}" --add-needed "libutils-v33.so" "${2}"
         ;;
     vendor/lib/hw/camera.msm8996.so)
         sed -i "s/service.bootanim.exit/service.bootanim.zzzz/g" "${2}"
@@ -78,7 +78,7 @@ function blob_fixup() {
         sed -i "s/vulkan.msm8953.so/vulkan.msm8996.so/g" "${2}"
         ;;
     vendor/lib64/libsettings.so)
-        "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+        "${PATCHELF_0_18}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
         ;;
     vendor/lib/libsymphony-1.1.1.so)
         "${PATCHELF_0_18}" --set-soname "libsymphony-1.1.1.so" "${2}"
@@ -87,8 +87,8 @@ function blob_fixup() {
         "${PATCHELF_0_18}" --set-soname "libsymphonypower-1.1.1.so" "${2}"
         ;;
     vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so)
-        "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
-         grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+        "${PATCHELF_0_18}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
+         grep -q libcrypto_shim.so "${2}" || "${PATCHELF_0_18}" --add-needed "libcrypto_shim.so" "${2}"
         ;;
     vendor/lib64/vulkan.msm8996.so)
         sed -i "s/vulkan.msm8953.so/vulkan.msm8996.so/g" "${2}"
