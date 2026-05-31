29fa017927d97b0b48cb5b65434c89db35e8b0a3
Alex <simutalexandru@yahoo.com>
2026-05-31 03:38:48 +0000

    msm8996-common: Use C2 Google media codec configs
    Change-Id: I828ed34323d7ebb129c03b895eaab0d1e3f5dfe0

--- msm8996.mk
+++ msm8996.mk
@@ -251,6 +251,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
