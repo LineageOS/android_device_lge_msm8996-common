88d64af34f7b7d102324df7bc50d78b42986300b
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-18 02:43:51 -0400

    msm8996-common: sepolicy: Move to vendor subdir
    Change-Id: Ia17ed2d87692afb3343d3d60cd7ed8d10fc43e82

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,7 +156,7 @@ BOARD_ROOT_EXTRA_FOLDERS := firmware persist persist-lg sns mpt
 # SELinux policies
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 include hardware/sony/timekeep/sepolicy/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/vendor/sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verity
