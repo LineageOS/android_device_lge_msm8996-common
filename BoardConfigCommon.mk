b7bf4502fc52d349811d03ec7eef4dac2969184b
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-18 02:46:13 -0400

    msm8996-common: sepolicy: private: Minimal allowances
    Change-Id: I8e4dda20c9efbe44e30f924d88fe25e9f63b3a20

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,6 +161,7 @@ BOARD_ROOT_EXTRA_FOLDERS := firmware persist persist-lg sns mpt
 # SELinux policies
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/vendor/sepolicy
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verity
